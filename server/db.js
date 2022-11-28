c84942f65a434f8e75fc742c9e3986cc72f8cc23
raytieu <ray.tieu@gmail.com>
2022-11-27 23:14:06 -0800

    finished v1.0

--- server/db.js
+++ server/db.js
@@ -2,9 +2,10 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
   user: "postgres",
-  password: "pgpassword",
+  password: "passwordpg",
   host: "localhost",
   port: 5432,
+  database: "perntodo",
 });
 
 module.exports = pool;
